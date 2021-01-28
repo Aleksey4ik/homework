443548bd053772795123ba6be1c5c563026c5c65
Aleksey! <Alekmog@yandex.ru>
2021-01-28 12:54:10 +0300

    Added task4

---
+++ lesson11/index4.php
@@ -0,0 +1,38 @@
+<?php 
+
+trait hasBirthday 
+{
+    private object $dateTime;
+
+    public function __construct(string $date)
+    {
+        $this->dateTime = new DateTime($date);
+    }
+
+    public function setDateTime($date): void
+    {
+        $this->dateTime = new DateTime($date);
+    }
+
+    public function getDateTime(): object
+    {
+        return $this->dateTime;
+    }    
+}
+
+class Person {
+    use hasBirthday;
+}
+
+class Pet {
+    use hasBirthday;
+}
+
+
+$person = new Person("2009-11-15");
+$person->setDateTime('2005-11-01');
+print_r($person->getDateTime());
+
+$pet = new Pet('2020-06-10');
+$pet->setDateTime('2015-07-07');
+print_r($pet->getDateTime());
