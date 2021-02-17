5947704b11eac19db33fc710fe15cfeec9b0f708
Aleksey! <Alekmog@yandex.ru>
2021-02-18 00:37:15 +0300

    add task about steps

---
+++ LeetCode/task2.php
@@ -0,0 +1,19 @@
+<?php
+
+// $arr = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1];
+$arr = [10, 15, 20];
+// $arr = [0, 0, 1, 1];
+
+
+function stepsBySteps(array $arr): int
+{
+    $arr[] = 0;
+    
+    for ($i=2; $i < count($arr); $i++) { 
+        $arr[$i] += min($arr[$i-1], $arr[$i-2]);
+    }
+
+    return $arr[count($arr) - 1];
+}
+
+echo stepsBySteps($arr);
